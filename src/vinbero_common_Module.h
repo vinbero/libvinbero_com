805fff9e2d0c8e6553c0b293138b3dfebd6a9ab7
Byeonggon Lee <gonny952@gmail.com>
2018-05-28 18:54:59 +0900

    needsChildren -> childrenRequired

--- src/vinbero_common_Module.h
+++ src/vinbero_common_Module.h
@@ -14,7 +14,7 @@ struct vinbero_common_Module {
     const char* id;
     const char* name;
     const char* version;
-    bool needsChildren;
+    bool childrenRequired;
     struct fastdl_Handle dlHandle;
     union genc_Generic localModule;
     pthread_rwlock_t* rwLock;
