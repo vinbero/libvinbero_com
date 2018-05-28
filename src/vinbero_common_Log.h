e9263596df1ddc3f4e4b63349012ae4663c3cbf0
Byeonggon Lee <gonny952@gmail.com>
2018-05-28 18:43:47 +0900

    VINBERO_COMMON_LOG_TRACE2

--- src/vinbero_common_Log.h
+++ src/vinbero_common_Log.h
@@ -11,6 +11,7 @@ int vinbero_common_Log_raw(int level, const char* source, int line, const char* 
 #define VINBERO_COMMON_LOG_LEVEL_FATAL 5
 
 #define VINBERO_COMMON_LOG_TRACE(...) vinbero_common_Log_raw(VINBERO_COMMON_LOG_LEVEL_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define VINBERO_COMMON_LOG_TRACE2(...) VINBERO_COMMON_LOG_TRACE("in %s()", __FUNCTION__)
 #define VINBERO_COMMON_LOG_DEBUG(...) vinbero_common_Log_raw(VINBERO_COMMON_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define VINBERO_COMMON_LOG_INFO(...) vinbero_common_Log_raw(VINBERO_COMMON_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__)
 #define VINBERO_COMMON_LOG_WARN(...) vinbero_common_Log_raw(VINBERO_COMMON_LOG_LEVEL_WARN, __FILE__, __LINE__, __VA_ARGS__)
