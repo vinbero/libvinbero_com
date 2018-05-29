d9c251729a42d22d7e9aaae38eded31aabfee5e9
Byeonggon Lee <gonny952@gmail.com>
2018-05-30 05:29:40 +0900

    Commit

--- src/vinbero_common_Dlsym.h
+++ src/vinbero_common_Dlsym.h
@@ -4,12 +4,11 @@
 #include <fastdl.h>
 #include "vinbero_common_Error.h"
 
-#define VINBERO_COMMON_DLSYM(interface, dlHandle, functionName, ret) do { \
-    if(fastdl_sym(dlHandle, #functionName, (void**)&(interface)->functionName) == -1) \
+#define VINBERO_COMMON_DLSYM(dlHandle, functionName, out, ret) do { \
+    if(fastdl_sym(dlHandle, functionName, out) == -1) \
         *(ret) = VINBERO_COMMON_EUNKNOWN; \
     else \
         *(ret) = 0; \
-} \
-while(0)
+} while(0)
 
 #endif
--- src/vinbero_common_Module.h
+++ src/vinbero_common_Module.h
@@ -1,12 +1,14 @@
 #ifndef _VINBERO_COMMON_MODULE_H
 #define _VINBERO_COMMON_MODULE_H
 
+#include <dlfcn.h>
 #include <fastdl.h>
 #include <pthread.h>
 #include <libgenc/genc_Generic.h>
 #include <libgenc/genc_Tree.h>
 #include <libgenc/genc_ArrayList.h>
 #include "vinbero_common_Config.h"
+#include "vinbero_common_Dlsym.h"
 
 struct vinbero_common_Module {
     const char* id;
@@ -26,6 +28,7 @@ struct vinbero_common_Module_Ids {
 
 int vinbero_common_Module_dlopen(struct vinbero_common_Module* module, struct vinbero_common_Config* config);
 
-#define VINBERO_COMMON_MODULE_DLSYM(interface, module, functionName, ret) VINBERO_COMMON_DLSYM(interface, module, functionName, ret)
+#define VINBERO_COMMON_MODULE_DLSYM(interface, dlHandle, functionName, ret) \
+VINBERO_COMMON_DLSYM(dlHandle, #functionName, (void**)&(interface)->functionName, ret)
 
 #endif
