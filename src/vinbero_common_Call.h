d39bf9b2209d712ec6b8669c03a14fc83a66416f
Byeonggon Lee <gonny952@gmail.com>
2018-06-02 19:06:48 +0900

    Fix bugs

--- src/vinbero_common_Call.h
+++ src/vinbero_common_Call.h
@@ -3,12 +3,12 @@
 #define VINBERO_COMMON_CALL(iName, fName, module, ret, ...) do { \
     struct vinbero_Interface_ ## iName interface; \
     interface.vinbero_Interface_ ## iName ## _ ## fName = NULL; \
-    VINBERO_COMMON_DLSYM(&(module)->dlHandle, #fName, &interface.vinbero_Interface_ ## iName ## _ ## fName, ret); \
+    VINBERO_COMMON_DLSYM(&(module)->dlHandle, "vinbero_Interface_"#iName"_"#fName, &interface.vinbero_Interface_ ## iName ## _ ## fName, ret); \
     if(*ret < 0) { \
-        VINBERO_COMMON_LOG_ERROR("VINBERO_%s_DLSYM() failed at module %s, with function %s", #iName, (module)->id, "vinbero_"#iName"_"#fName); \
+        VINBERO_COMMON_LOG_WARN("VINBERO_%s_DLSYM() failed at module %s, with function %s", #iName, (module)->id, "vinbero_Interface_"#iName"_"#fName); \
         break; \
     } \
     if(((*ret) = interface.vinbero_Interface_ ## iName ## _ ## fName (__VA_ARGS__)) < 0) \
-        VINBERO_COMMON_LOG_WARN("vinbero_%s_%s() failed at module %s", #iName, #fName, (module)->id); \
+        VINBERO_COMMON_LOG_WARN("vinbero_Interface_%s_%s() failed at module %s", #iName, #fName, (module)->id); \
 } while(0)
 #endif
