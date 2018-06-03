c1fb0a2415eb5c80604e84aba395b6d64d8ee5b9
Byeonggon Lee <gonny952@gmail.com>
2018-06-03 19:01:58 +0900

    Commit

--- src/vinbero_common_Call.h
+++ src/vinbero_common_Call.h
@@ -2,7 +2,6 @@
 #define _VINBERO_COMMON_CALL
 #define VINBERO_COMMON_CALL(iName, fName, module, ret, ...) do { \
     struct vinbero_Interface_ ## iName interface; \
-    interface.vinbero_Interface_ ## iName ## _ ## fName = NULL; \
     VINBERO_COMMON_DLSYM(&(module)->dlHandle, "vinbero_Interface_"#iName"_"#fName, &interface.vinbero_Interface_ ## iName ## _ ## fName, ret); \
     if(*ret < 0) { \
         VINBERO_COMMON_LOG_WARN("VINBERO_%s_DLSYM() failed at module %s, with function %s", #iName, (module)->id, "vinbero_Interface_"#iName"_"#fName); \
