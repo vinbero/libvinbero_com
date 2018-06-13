93a6da337a49d1ed374863d221f0f37b6db69fe9
Byeonggon Lee <gonny952@gmail.com>
2018-06-13 12:19:12 +0900

    Stop using mutex in logging

--- src/vinbero_common_Log.c
+++ src/vinbero_common_Log.c
@@ -9,9 +9,6 @@
 
 static int vinbero_common_Log_flag = 0; 
 
-static pthread_mutex_t vinbero_common_Log_mutex;
-static bool vinbero_common_Log_mutexInitialized = false;
-
 void vinbero_common_Log_printLogLevelInfo(int flag) {
     if(flag & VINBERO_COMMON_LOG_FLAG_TRACE)
         VINBERO_COMMON_LOG_INFO("TRACE LEVEL LOGGING ENABLED");
@@ -51,27 +48,22 @@ static int vinbero_common_Log_levelToFlag(int level) {
 
 int vinbero_common_Log_init(int flag) {
     vinbero_common_Log_flag = flag;
-    if(pthread_mutex_init(&vinbero_common_Log_mutex, NULL) == -1)
-        return -errno;
-    vinbero_common_Log_mutexInitialized = true;
     return 0;
 }
 
 void vinbero_common_Log_raw(int level, const char* source, int line, const char* format, ...) {
-    if(!vinbero_common_Log_mutexInitialized)
-        return;
-    pthread_mutex_lock(&vinbero_common_Log_mutex);
     if((vinbero_common_Log_levelToFlag(level) & vinbero_common_Log_flag) != 0) {
         time_t t = time(NULL);
         struct tm now;
         localtime_r(&t, &now);
-        fprintf(stderr, "\x1B[1;30m[%02d/%02d/%d/%02d:%02d:%02d]\x1B[0m ", now.tm_mday, now.tm_mon + 1, now.tm_year + 1900, now.tm_hour, now.tm_min, now.tm_sec);
-        fprintf(stderr, "%s %s: %d: ", vinbero_common_Log_levelString(level), source, line);
         va_list args;
         va_start(args, format);
+        flockfile(stderr);
+        fprintf(stderr, "\x1B[1;30m[%02d/%02d/%d/%02d:%02d:%02d]\x1B[0m ", now.tm_mday, now.tm_mon + 1, now.tm_year + 1900, now.tm_hour, now.tm_min, now.tm_sec);
+        fprintf(stderr, "%s %s: %d: ", vinbero_common_Log_levelString(level), source, line);
         vfprintf(stderr, format, args);
-        va_end(args);
         fprintf(stderr, "\n");
+        funlockfile(stderr);
+        va_end(args);
     }
-    pthread_mutex_unlock(&vinbero_common_Log_mutex);
 }
