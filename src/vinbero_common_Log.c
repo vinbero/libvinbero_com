dd61056651304cd190b2272aa72bcf933e81fab0
Byeonggon Lee <gonny952@gmail.com>
2018-12-12 21:45:27 +0900

    Show tid on logging

--- src/vinbero_common_Log.c
+++ src/vinbero_common_Log.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 #include <pthread.h>
 #include "vinbero_common_Log.h"
 
@@ -81,6 +83,7 @@ void vinbero_common_Log_raw(int level, const char* source, int line, const char*
             fprintf(stderr, "\x1B[1;30m[%02d/%02d/%d/%02d:%02d:%02d]\x1B[0m ", now.tm_mday, now.tm_mon + 1, now.tm_year + 1900, now.tm_hour, now.tm_min, now.tm_sec);
         else
             fprintf(stderr, "[%02d/%02d/%d/%02d:%02d:%02d] ", now.tm_mday, now.tm_mon + 1, now.tm_year + 1900, now.tm_hour, now.tm_min, now.tm_sec);
+        fprintf(stderr, "[%lu] ", syscall(SYS_gettid));
         fprintf(stderr, "%s ", vinbero_common_Log_levelString(level));
         if(vinbero_common_Log_option & VINBERO_COMMON_LOG_OPTION_SOURCE)
            fprintf(stderr, "%s: %d: ", source, line);
