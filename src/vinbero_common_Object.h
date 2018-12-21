611e2820ea6bd57f406b506ad747277a5ff23ea7
Byeonggon Lee <gonny952@gmail.com>
2018-12-21 16:57:47 +0000

    Modified vinbero_common_Object

--- src/vinbero_common_Object.h
+++ src/vinbero_common_Object.h
@@ -2,57 +2,67 @@
 #define _VINBERO_COMMON_OBJECT_H
 
 struct vinbero_common_Object {
+    union data {
+       bool boolean;
+       int integer;
+       double real;
+       char* string;
+       GENC_TREE_NODE(struct vinbero_common_Object, struct vinbero_common_Object*);
+       // GENC_HTREE_NODE(struct vinbero_common_Object, struct vinbero_common_Object*);
+    };
+    enum vinbero_common_Object_Type type;
     json_t* json;
 };
 
-#define VINBERO_COMMON_OBJECT_IS_BOOL(object) \
-json_is_boolean((object)->json)
+#define VINBERO_COMMON_OBJECT_IS_BOOLEAN(object) \
+object->type == VINBERO_COMMON_OBJECT_TYPE_BOOLEAN
 
-#define VINBERO_COMMON_OBJECT_IS_INT(object) \
-json_is_integer((object)->json)
+#define VINBERO_COMMON_OBJECT_IS_INTEGER(object) \
+object->type == VINBERO_COMMON_OBJECT_TYPE_INTEGER
 
-#define VINBERO_COMMON_OBJECT_IS_DOUBLE(object) \
-json_is_real((object)->json)
+#define VINBERO_COMMON_OBJECT_IS_REAL(object) \
+object->type == VINBERO_COMMON_OBJECT_TYPE_REAL
 
 #define VINBERO_COMMON_OBJECT_IS_STRING(object) \
-json_is_string((object)->json)
+object->type == VINBERO_COMMON_OBJECT_TYPE_STRING
 
 #define VINBERO_COMMON_OBJECT_IS_ARRAY(object) \
-json_is_array((object)->json)
+object->type == VINBERO_COMMON_OBJECT_TYPE_ARRAY
 
 #define VINBERO_COMMON_OBJECT_IS_MAP(object) \
-json_is_object((object)->json)
+object->type == VINBERO_COMMON_OBJECT_TYPE_MAP
 
-#define VINBERO_COMMON_OBJECT_EVAL_BOOL(object) \
-json_boolean_value((object)->json)
+#define VINBERO_COMMON_OBJECT_EVAL_BOOLEAN(object) \
+object->data.boolean
 
-#define VINBERO_COMMON_OBJECT_EVAL_INT(object, value, ret) \
-json_integer_value((object)->json)
+#define VINBERO_COMMON_OBJECT_EVAL_INTEGER(object) \
+object->data.integer
 
-#define VINBERO_COMMON_OBJECT_EVAL_DOUBLE(object, value, ret) \
-json_real_value((object)->json)
+#define VINBERO_COMMON_OBJECT_EVAL_REAL(object) \
+object->data.real
 
 #define VINBERO_COMMON_OBJECT_EVAL_STRING(object, value, ret) \
-json_string_value((object)->json)
+object->data.string
 
-#define VINBERO_COMMON_OBJECT_ARRAY_GET(object, index, ret) \
-json_array_get((object)->json, index)
+#define VINBERO_COMMON_OBJECT_ARRAY_GET(object, index) \
+GENC_TREE_NODE_GET_CHILD(object, index)
 
 #define VINBERO_COMMON_OBJECT_ARRAY_GET_SIZE(object) \
-json_array_size((object)->json)
+GENC_TREE_NODE_GET_CHILD_COUNT(object)
 
-#define VINBERO_COMMON_OBJECT_MAP_GET(object, key, ret) \
-json_object_get((object)->json, key)
+#define VINBERO_COMMON_OBJECT_MAP_GET(object, key) \
+GENC_HTREE_NODE_GET_CHILD(object, key)
 
 #define VINBERO_COMMON_OBJECT_ARRAY_FOR_EACH_BEGIN(object, index, value) do { \
-    json_array_foreach() {
+    GENC_TREE_NODE_FOR_EACH_CHILD(object, index) {
 
-#define VINBERO_COMMON_OBJECT_ARRAY_FOR_EACH_END(object, index, value) \
+#define VINBERO_COMMON_OBJECT_ARRAY_FOR_EACH_END \
     } \
 } while(0)
 
 #define VINBERO_COMMON_OBJECT_MAP_FOR_EACH_BEGIN(object, key, value) do { \
-    json_object_foreach() { \
+    GENC_HTREE_NODE_FOR_EACH_CHILD(object, key) { \
+
 #define VINBERO_COMMON_OBJECT_MAP_FOR_EACH_END \
     } \
 } while(0)
