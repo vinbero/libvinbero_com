7192ad83c0c3144e709dace82cf4c7b2336c8bd5
Byeonggon Lee <gonny952@gmail.com>
2019-01-24 06:36:54 +0000

    Added path in struct vinbero_common_Module, Deleted VINBERO_COMMON_MODULE_DLOPEN()

--- src/vinbero_common_Module.h
+++ src/vinbero_common_Module.h
@@ -18,6 +18,7 @@ struct vinbero_common_Module {
     bool childrenRequired;
     struct vinbero_common_Config* config;
     const char* id;
+    const char* path;
     struct fastdl_Handle dlHandle;
     union genc_Generic localModule;
     void* arg; // arg from parentModule
@@ -25,31 +26,6 @@ struct vinbero_common_Module {
 };
 
 void vinbero_common_Module_init(struct vinbero_common_Module* module, const char* name, const char* version, bool childrenRequired);
-
-#define VINBERO_COMMON_MODULE_DLOPEN(module, ret) do { \
-    *(ret) = VINBERO_COMMON_STATUS_SUCCESS; \
-    struct vinbero_common_Object* paths; \
-    VINBERO_COMMON_CONFIG_MGET_REQ((module)->config, module, "paths", ARRAY, &paths); \
-    if(paths == NULL) { \
-        *(ret) = VINBERO_COMMON_ERROR_INVALID_CONFIG; \
-        break; \
-    } \
-    if(GENC_TREE_NODE_CHILD_COUNT(paths) < 1) { \
-        *(ret) = VINBERO_COMMON_ERROR_INVALID_CONFIG; \
-        break; \
-    } \
-    GENC_TREE_NODE_FOR_EACH_CHILD(paths, index) { \
-        struct vinbero_common_Object* path = GENC_TREE_NODE_GET_CHILD(paths, index); \
-        if(!VINBERO_COMMON_OBJECT_IS_CONSTRING(path)) { \
-            *(ret) = VINBERO_COMMON_ERROR_INVALID_CONFIG; \
-	    break; \
-        } \
-        if(fastdl_open(&(module)->dlHandle, VINBERO_COMMON_OBJECT_CONSTRING(path), RTLD_LAZY | RTLD_GLOBAL) == -1) { \
-            *(ret) = VINBERO_COMMON_ERROR_DLOPEN; \
-            break; \
-        } \
-    } \
-} while(0)
 
 #define VINBERO_COMMON_MODULE_DLSYM(interface, dlHandle, functionName, ret) do { \
     (interface)->functionName = NULL; \
