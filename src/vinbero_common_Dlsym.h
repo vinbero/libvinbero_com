58127ba9e570a1492d7392eb386927608c234def
Byeonggon Lee <gonny952@gmail.com>
2019-01-24 06:03:36 +0000

    Use paths instead of path in config

--- src/vinbero_common_Dlsym.h
+++ src/vinbero_common_Dlsym.h
@@ -3,13 +3,14 @@
 
 #include <fastdl.h>
 #include "vinbero_common_Error.h"
+#include "vinbero_common_Status.h"
 
 #define VINBERO_COMMON_DLSYM(dlHandle, functionName, out, ret) do { \
     FASTDL_SYM(dlHandle, functionName, out, ret); \
     if(*(ret) == -1) \
         *(ret) = VINBERO_COMMON_ERROR_DLSYM; \
     else \
-        *(ret) = 0; \
+        *(ret) = VINBERO_COMMON_STATUS_SUCCESS; \
 } while(0)
 
 #endif
